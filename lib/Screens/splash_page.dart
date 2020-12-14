9629e88c855b3525d47ca03d3c3bb478dd50dfbe
Nishank00 <nishanksidhpura@gmail.com>
2020-12-14 22:50:16 +0530

    Initial Commit

---
+++ lib/Screens/splash_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Loading..."),
+      ),
+    );
+  }
+}
